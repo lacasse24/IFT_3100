1d0a2c8ecf67d2997a0a5bfaccdf3370638303c3
Brandon <dieusaistout>
2019-03-06 13:50:17 -0500

    Added a bunch of utility functions to the Transform class

--- Transform.cpp
+++ Transform.cpp
@@ -4,9 +4,90 @@
 
 Transform::Transform()
 {
+
 }
 
 
 Transform::~Transform()
 {
+
+}
+
+void Transform::translate(aiVector3D pos)
+{
+	_position += pos;
+}
+
+void Transform::rotate(aiVector3D rot, float a)
+{
+	aiQuaternion q = aiQuaternion(rot, a);
+	q.Rotate(_rotation);
+}
+
+void Transform::scale(aiVector3D sca)
+{
+	scale(sca.x, sca.y, sca.z);
+}
+
+void Transform::translate(float x, float y, float z)
+{
+	translate(aiVector3D(x, y, z));
+}
+
+void Transform::rotate(float x, float y, float z, float a)
+{
+	aiQuaternion q = aiQuaternion(aiVector3D(x, y, z), a);
+	q.Rotate(_rotation);
+}
+
+void Transform::scale(float x, float y, float z)
+{
+	_scale.x *= x;
+	_scale.y *= y;
+	_scale.z *= z;
+}
+
+aiVector3D Transform::getPosition() const
+{
+	return _position;
+}
+
+aiVector3D Transform::getRotation() const
+{
+	return _rotation;
+}
+
+aiVector3D Transform::getScale() const
+{
+	return _scale;
+}
+
+void Transform::setPosition(aiVector3D pos)
+{
+	_position = pos;
+}
+
+void Transform::setRotation(aiVector3D rot)
+{
+	_rotation = rot;
+}
+
+void Transform::setScale(aiVector3D sca)
+{
+	_scale = sca;
+}
+
+void Transform::setPosition(float x, float y, float z)
+{
+	_position = aiVector3D(x, y, z);
+}
+
+void Transform::setRotation(float x, float y, float z)
+{
+	_rotation = aiVector3D(x, y, z);
+}
+
+void Transform::setScale(float x, float y, float z)
+{
+	_scale = aiVector3D(x, y, z);
 }
--- Transform.h
+++ Transform.h
@@ -1,12 +1,38 @@
 #pragma once
 
 #include <vector3.h>
+#include <quaternion.h>
 
 class Transform
 {
 public:
 	Transform();
 	~Transform();
+
+	//Transformation modification methods using a vector3
+	void translate(aiVector3D pos);
+	void rotate(aiVector3D rot, float a);
+	void scale(aiVector3D sca);
+
+	//Transformation modification methods using x, y, z
+	void translate(float x, float y, float z);
+	void rotate(float x, float y, float z, float a);
+	void scale(float x, float y, float z);
+
+	//Getters
+	aiVector3D getPosition() const;
+	aiVector3D getRotation() const;
+	aiVector3D getScale() const;
+
+	//Setters using a vector3
+	void setPosition(aiVector3D pos);
+	void setRotation(aiVector3D rot);
+	void setScale(aiVector3D sca);
+
+	//Setter using x, y, z
+	void setPosition(float x, float y, float z);
+	void setRotation(float x, float y, float z);
+	void setScale(float x, float y, float z);
 
 private:
 	aiVector3D _position;
