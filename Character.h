687878a78545e67fe938d27606a92e7c251888c4
Brandon <dieusaistout>
2019-03-06 23:33:07 -0500

    Added some Character attributes

--- Character.h
+++ Character.h
@@ -1,10 +1,21 @@
 #pragma once
 #include "GameObject.h"
+#include "Holdable.h"
+#include "Wearable.h"
 
 class Character : public GameObject
 {
 public:
 	Character();
 	virtual ~Character();
+
+private:
+	Holdable _lefthand;
+	Holdable _righthand;
+
+	Wearable _helmet;
+	Wearable _cape;
+	Wearable _armor;
+	Wearable _boots;
 };
 
